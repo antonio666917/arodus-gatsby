2f64c8c83d41cb463b14da786cab38a912280c21
Antonio Rodríguez <antoniwan@gmail.com>
2017-10-06 09:39:47 -0400

    Add base font styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,29 @@
 import Typography from 'typography';
 
+const headerGoogleFont = 'Source Sans Pro';
+const bodyGoogleFont = 'Open Sans';
+
 const typography = new Typography({
   baseFontSize: '16px',
+  baseLineHeight: 1.666,
+  scaleRatio: 1.666,
+  headerFontFamily: [headerGoogleFont, 'sans-serif'],
+  bodyFontFamily: [bodyGoogleFont, 'sans-serif'],
+  googleFonts: [
+    {
+      name: bodyGoogleFont,
+      styles: [
+        '300',
+        '400'
+      ],
+    },
+    {
+      name: headerGoogleFont,
+      styles: [
+        '500'
+      ]
+    }
+  ],
 });
 
 export default typography;
