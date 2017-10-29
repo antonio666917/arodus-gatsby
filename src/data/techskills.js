eceb145e91fcfbef6c2d7f48eac46af87806d9e0
Antonio Rodríguez <antoniwan@gmail.com>
2017-10-29 18:29:23 -0400

    Add a tools component to showcase tech skills

---
+++ src/data/techskills.js
@@ -0,0 +1,338 @@
+export default {
+  TechSkills: [
+    {
+      title: "PHP",
+      desc: "server-side programming langauge",
+      prod: true,
+      hobby: true,
+      actual: false
+    },
+    {
+      title: "NodeJS",
+      desc: "javaScript server-side runtime back-end",
+      prod: true,
+      hobby: true,
+      actual: true,
+      learning: true
+    },
+    {
+      title: "Git",
+      desc: "source code versioning",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "Laravel PHP",
+      desc: "modern PHP framework",
+      prod: true,
+      hobby: true,
+      actual: false
+    },
+    {
+      title: "Gulp",
+      desc: "automation toolkit (task runner)",
+      prod: true,
+      actual: false,
+      hobby: true
+    },
+    {
+      title: "Grunt",
+      desc: "javascript Task Runner",
+      prod: true,
+      hobby: true,
+      actual: false
+    },
+    {
+      title: "Webpack",
+      desc: "javascript Module bundler",
+      prod: true,
+      hobby: true,
+      actual: true,
+      learning: true
+    },
+    {
+      title: "TypeScript",
+      desc: "typed superset of Javascript",
+      prod: true,
+      hobby: false,
+      actual: false
+    },
+    {
+      title: "AngularJS",
+      desc: "front-end JavaScript library framework",
+      prod: true,
+      hobby: true,
+      actual: false
+    },
+    {
+      title: "Angular",
+      desc: "monolithic JavaScript framework",
+      prod: true,
+      hobby: false,
+      actual: false
+    },
+    {
+      title: "React",
+      desc: "javaScript library for building user interfaces",
+      prod: true,
+      hobby: true,
+      actual: true,
+      learning: true
+    },
+    {
+      title: "MySQL",
+      desc: "relational database system",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "SCSS",
+      desc: "style sheet language",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "Nginx",
+      desc: "modern server",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "Python",
+      desc: "server-side programming language",
+      prod: false,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "Scrum",
+      desc: "agile framework for completing complex projects",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "React Native",
+      desc: "build native mobile apps using JavaScript and React",
+      prod: false,
+      hobby: true,
+      actual: true,
+      learning: true
+    },
+    {
+      title: "Google Cloud Platform",
+      desc: "cloud-based scalable infrastructure",
+      prod: true,
+      hobby: false,
+      actual: true
+    },
+    {
+      title: "Heroku",
+      desc: "cloud-based application hosting",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "Digital Ocean",
+      desc: "cloud computing platform",
+      prod: true,
+      hobby: true
+    },
+    {
+      title: "Google Analytics",
+      desc: "web analytics service",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "Adobe Analytics (Omniture)",
+      desc: "web analytics service",
+      prod: true,
+      hobby: false,
+      actual: true
+    },
+    {
+      title: "CoffeeScript",
+      desc: "javaScript subset language",
+      prod: true,
+      hobby: true,
+      legacy: true,
+      actual: false
+    },
+    {
+      title: "Sketch",
+      desc: "design toolkit",
+      prod: true,
+      hobby: true
+    },
+    {
+      title: "Amazon Alexa",
+      desc: "voice service framework",
+      prod: true,
+      hobby: true
+    },
+    {
+      title: "Wit.AI",
+      desc: "natural language API",
+      prod: true,
+      hobby: true
+    },
+    {
+      title: "Visual Studio Code",
+      desc: "text editor",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "Zeit's Now",
+      desc: "realtime global deployments",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "Go Language",
+      desc: "programming language",
+      prod: false,
+      hobby: true,
+      actual: true,
+      learning: true
+    },
+    {
+      title: "Apache Cordova",
+      desc: "mobile development framework",
+      prod: true,
+      hobby: true,
+      actual: false,
+      learning: false
+    },
+    {
+      title: "Lumen PHP",
+      desc: "minimal PHP framework",
+      prod: true,
+      hobby: false,
+      actual: false
+    },
+    {
+      title: "ES6",
+      desc: "javaScript language update",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "ES7",
+      desc: "javaScript language update",
+      prod: true,
+      hobby: true,
+      actual: true,
+      learning: true
+    },
+    {
+      title: "Sublime Text 2/3",
+      desc: "text editor",
+      prod: true,
+      hobby: true,
+      actual: false
+    },
+    {
+      title: "JavaScript",
+      desc: "glorious programming language",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "VIM",
+      desc: "terminal text editor",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "Mailgun",
+      desc: "transactional email API",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "SendGrid",
+      desc: "transactional email API",
+      prod: true,
+      hobby: true,
+      actual: false
+    },
+    {
+      title: "Terminal & CLIs",
+      desc: "command-line interfaces",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "WordPress",
+      desc: "PHP website & CMS software",
+      prod: true,
+      hobby: true,
+      actual: false
+    },
+    {
+      title: "Vagrant",
+      desc: "development environments",
+      prod: true,
+      hobby: true,
+      actual: false
+    },
+    {
+      title: "Docker",
+      desc: "containerization platform",
+      prod: false,
+      hobby: false,
+      learning: true,
+      actual: false
+    },
+    {
+      title: "Microsoft Bot Framework",
+      desc: "framework for building chatbots",
+      prod: true,
+      hobby: true,
+      actual: false
+    },
+    {
+      title: "Facebook Graph & Marketing APIs",
+      desc: "Facebook integrations SDKs",
+      prod: true,
+      hobby: true,
+      actual: true
+    },
+    {
+      title: "PostgreSQL",
+      desc: "database system",
+      prod: true,
+      hobby: false,
+      actual: false
+    },
+    {
+      title: "Let's Encrypt",
+      desc: "certificate authority",
+      prod: true,
+      hobby: false,
+      actual: true
+    },
+    {
+      title: "Firebase",
+      desc: "mobile and web application development platform",
+      prod: true,
+      hobby: true,
+      actual: true
+    }
+  ]
+};
---
+++ src/pages/technology.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Tools from "../components/Tools";
+
+class TechBox extends React.Component {
+  render() {
+    return <Tools />;
+  }
+}
+export default TechBox;
