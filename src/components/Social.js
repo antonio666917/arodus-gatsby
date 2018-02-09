94cb0d7b81bde8392878817ff2a635b3ce2ddbf5
Antonio Rodriguez <antoniwan@gmail.com>
2018-02-09 13:46:58 -0500

    Update social links

--- src/components/Social.js
+++ src/components/Social.js
@@ -8,20 +8,25 @@ import FaGithub from "react-icons/lib/fa/github-alt";
 import FaTwitter from "react-icons/lib/fa/twitter-square";
 import FaFacebook from "react-icons/lib/fa/facebook-official";
 import FaLastfm from "react-icons/lib/fa/lastfm-square";
-import Mail from "react-icons/lib/go/mail-read";
+import FaMail from "react-icons/lib/go/mail-read";
+import FaCodePen from "react-icons/lib/fa/codepen";
+import FaSpotify from "react-icons/lib/fa/spotify";
 
 const SocialGrid = styled.section`
+  width: 100%;
   .social-links {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+    grid-gap: 0.5rem;
+    align-items: center;
     a {
       color: #b1b1b1;
       display: block;
-      flex-basis: 30%;
+      font-size: 1rem;
+      line-height: 1.2rem;
+      padding: 0.5rem;
       span {
-        margin-left: 0.5rem;
+        margin-left: 0.2rem;
       }
       &:hover {
         color: white;
@@ -33,6 +38,24 @@ const SocialGrid = styled.section`
 export default () => (
   <SocialGrid>
     <div className="social-links">
+      <a
+        href="https://twitter.com/antoniwan"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaTwitter />
+        <span>Twitter</span>
+      </a>
+
+      <a
+        href="http://instagram.com/antoniwan"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram />
+        <span>Instagram</span>
+      </a>
+
       <a
         href="https://www.linkedin.com/in/antoniwan"
         target="_blank"
@@ -52,12 +75,12 @@ export default () => (
       </a>
 
       <a
-        href="https://twitter.com/antoniwan"
+        href="https://codepen.io/antoniwan/#"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaTwitter />
-        <span>Twitter</span>
+        <FaCodePen />
+        <span>CodePen</span>
       </a>
 
       <a
@@ -70,15 +93,6 @@ export default () => (
       </a>
 
       <a
-        href="http://instagram.com/antoniwan"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaInstagram />
-        <span>Instagram</span>
-      </a>
-
-      <a
         href="http://www.last.fm/user/antoniwan"
         target="_blank"
         rel="noopener noreferrer"
@@ -87,8 +101,17 @@ export default () => (
         <span>LastFM</span>
       </a>
 
+      <a
+        href="https://open.spotify.com/user/antoniwan"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaSpotify />
+        <span>Spotify</span>
+      </a>
+
       <a href="mailto:hello@antoniorodriguez.us">
-        <Mail />
+        <FaMail />
         <span>Email</span>
       </a>
     </div>
